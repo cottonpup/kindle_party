8e8ed7e99db20c24f969e8da0616289af5207029
cottonpup <younggyounglee1@gmail.com>
2022-03-31 00:50:24 +0900

    Add popup component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import './App.css';
-
+import Card from './components/UI/Card';
 
 function App() {
   return (
-<>
-  <h1>Create a fsafdas</h1>
-  <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-</>
+    <>
+      <Card>
+        <h1 className="text-gray-800 font-extrabold text-2xl pb-10">
+          Create a Kindle Party
+        </h1>
+        <div className="flex items-center pb-5 justify-between">
+          <p className="pr-2 text-gray-600 text-base leading-7 font-bold">
+            Only I have control
+          </p>
+          <div className="w-10 h-5 bg-gray-300 rounded-full flex items-center">
+            <div className="bg-white w-3 h-3 rounded-full shadow-md ml-1"></div>
+          </div>
+        </div>
+        <button className="w-full uppercase px-4 py-2 font-semibold text-sm bg-blue-400 text-white rounded-md shadow-sm opacity-100">
+          Start the party
+        </button>
+      </Card>
+    </>
   );
 }
 
--- src/components/UI/Card.jsx
+++ src/components/UI/Card.jsx
@@ -1,7 +1,9 @@
-
-
 const Card = (props) => {
-  return <div >{props.children}</div>;
+  return (
+    <div className="w-96 px-6 py-6 bg-white shadow-xl ring-1 ring-gray-900/5 rounded-md">
+      {props.children}
+    </div>
+  );
 };
 
 export default Card;
